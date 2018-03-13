0f0d517a33a2191270442cc47976fddc8f352dd5
Patrycja Kania <el.czupakabra21@gmail.com>
2018-03-14 00:13:38 +0100

    Fix script.js

--- script.js
+++ script.js
@@ -8,17 +8,5 @@ function drawTree (height) {
         console.log(star);
     } 
 }
-
 drawTree(prompt("wysokość choinki: "));
 
-
-function drawTree (height) {
-   for (var i = 1; i<=height; i++) {
-        var star = '';
-        for (var j = 1; j <= i/*ile razy wykonuje się pętla*/; j=j+2) {
-            star+='*';
-        }
-        console.log(star);
-    } 
-}
-drawTree(prompt("wysokość choinki: "));

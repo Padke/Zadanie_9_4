33085e0aaa46bf373ac363835e8cf560f1fe0997
Patrycja Kania <el.czupakabra21@gmail.com>
2018-03-14 18:25:45 +0100

    Fix script1.js

--- script1.js
+++ script1.js
@@ -1,13 +1,13 @@
 
 function drawTree (height) {
-   for (var i = 0; i <= height; i++) {
+   for (var i = 1; i <= height; i++) {
        var space = '';
-        for (var y = 0; y <= height - i; y++) {
+        for (var y = 1; y <= height - i; y++) {
             space+=' ';
         }
        
         var star = '';
-        for (var j = 1; j <= i*2+1; j++) {
+        for (var j = 1; j <= i*2-1; j++) {
             star+='*';
         }
         console.log(space + star);
